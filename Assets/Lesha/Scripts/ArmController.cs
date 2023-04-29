bbe16b55b8d8631487cc773934f6ea3c04338890
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-04-29 19:14:04 +0300

    Добавил ограничение
    Теперь нельзя двигать сразу двумя руками

--- Assets/Lesha/Scripts/ArmController.cs
+++ Assets/Lesha/Scripts/ArmController.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class ArmController : MonoBehaviour
 {
+    public bool IsMoving { get; private set; }
+
     [Range(0,1)][SerializeField] private int _button;
     [SerializeField] private GameObject _target;
     [SerializeField] private LayerMask _hitchLayerMask;
     [SerializeField] private Transform _deafultPosition;
     [SerializeField] private GameObject _limbSolver;
     [SerializeField] private GameObject _otherLimbSolver;
+    [SerializeField] private ArmController _otherArmController;
     [SerializeField] private float _grappingRadius;
 
-    private bool _isMoving;
     private DistanceJoint2D _joint;
 
     void Start()
     {
-        
-        _isMoving = false;
+
+        IsMoving = false;
         _joint = _target.GetComponent<DistanceJoint2D>();
     }
 
@@ -27,10 +29,10 @@ public class ArmController : MonoBehaviour
         transform.position = _target.transform.position;
         if (Input.GetMouseButtonDown(_button))
             StartMove();
-        if (Input.GetMouseButtonUp(_button) && _isMoving)
+        if (Input.GetMouseButtonUp(_button) && IsMoving)
             TryCling();
 
-        if (_isMoving) Move();
+        if (IsMoving) Move();
     }
 
     private void Move()
@@ -52,15 +54,15 @@ public class ArmController : MonoBehaviour
             _limbSolver.SetActive(false);
         }
 
-        _isMoving = false;
+        IsMoving = false;
     }
 
     private void StartMove()
     {
-        if (_otherLimbSolver.activeSelf)
+        if (_otherLimbSolver.activeSelf && !_otherArmController.IsMoving)
         {
             Debug.Log("StartMove");
-            _isMoving = true;
+            IsMoving = true;
             if (_joint.enabled == false) _joint.enabled = true;
             if (!_limbSolver.activeSelf) _limbSolver.SetActive(true);
         }
