0239208bf2fed0e1d81a6f7c526d9f386d37325f
Stepan <88424384+MulenokSA@users.noreply.github.com>
2023-05-01 18:20:54 +0300

    Переключение фона

--- Assets/Scripts/BGReplacer.cs
+++ Assets/Scripts/BGReplacer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class BGReplacer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        spriteRenderer.sprite = GameManager.instance.currentLevel.backGround;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
